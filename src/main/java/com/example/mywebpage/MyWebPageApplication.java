18e1c699f1767272d974e15cf111db0fc61ac45b
KeeUi0 <77387339+KeeUi0@users.noreply.github.com>
2024-05-17 18:34:32 +0900

    변경

---
+++ src/main/java/com/example/mywebpage/MyWebPageApplication.java
@@ -0,0 +1,13 @@
+package com.example.mywebpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyWebPageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mywebpage/MyWebPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mywebpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebPageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
